d7df2eb2d748a01aff0d24a1aa3aeac6c8869243
Yasir323 <yasir.jafri007@gmail.com>
2023-01-28 19:57:51 +0530

    Tests - ERROR

--- restful/drones/views.py
+++ restful/drones/views.py
@@ -3,7 +3,7 @@ Book: Django RESTful Web Services
 Author: Gaston C. Hillar - Twitter.com/gastonhillar
 Publisher: Packt Publishing Ltd. - http://www.packtpub.com
 """
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
